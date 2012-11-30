d5fb9a04f390cfee9d3acfe5dd171f463a44ae90
Eugene Kornykhin <kornevgen@ispras.ru>
2012-11-30 15:20:27 +0400

    Some heurictics are implemented

--- traverse_c.py
+++ traverse_c.py
@@ -4,6 +4,17 @@ import random
 import time
 import sys
 
+## heuristics implemented:
+##+ 1. miss(x) => x != y_1 /\ x != y_2 /\ ... /\ x != y_n where {y_i} = L1 before miss
+##+ 2. L_i has clique with 5 elements => unsat
+##+ 3. L_i has clique with 4 elements and some x != y_1 /\ x != y_2 /\ ... /\ x != y_{w-1} with {y_i} = this clique => x == y_w
+##+ 4. template has miss/hit/any(x) and after that miss/any(x_w) and then miss(x) and {x_1, x_2, ..., x_n} - literally different addresses between these memory accesses =>
+##	if n < w then unsat
+##	if n == w then any(x_w) == miss(x_w) and all {x, x_1, x_2, ..., x_w} pairly different !!!
+##+ 5. any(x) --> hit(x) if x in L_i before any(x)
+##  6. any(x) and L from this any(x) has a clique4 and x ~isin this clique4  => any(x) == hit(x)
+##+ 7. miss(x) and then hit(y) and x != y then  add y to addresses _before_ miss(x)
+
 def traverse_c(program, l1, equals, not_equals) :
 	for l in l1 :
 		assert isinstance(l, int)
@@ -32,10 +43,12 @@ def traverse_c(program, l1, equals, not_equals) :
 	
 	# try heuristics: 1 (may infer some inequalities and add them to not_equals)
 	while True :
+		print("heuristic 1: addresses in states of L1")
 		addrs = reachable_addresses(program, len(l0), equals, not_equals)
 		print("ADDRS = ", addrs)
 		if not sat(equals, not_equals) :
 			return False
+		print("heuristic 1.2: search for cliques5")
 		for ads in addrs :
 			if len(cliques(ads, len(l1) + 1, equals, not_equals)) > 0 :
 				return False
@@ -43,9 +56,21 @@ def traverse_c(program, l1, equals, not_equals) :
 		if not sat(equals, not_equals) :
 			return False
 	
-		if not infer(addrs, equals, not_equals) :
-#			print("NOT INFER :(")
-			break
+		print("heuristic 1.3 : infer equalities from cliques4")
+		if infer(addrs, equals, not_equals) :
+			if not sat(equals, not_equals) :
+				return False
+			continue
+
+		print("infer all_diffs")
+		if infer_alldiffs(program, len(l1), equals, not_equals) :
+			print("INFERRED!!! not_equals = ", not_equals)
+			if not sat(equals, not_equals) :
+				return False
+			continue
+
+#		print("NOT INFER :(")
+		break
 	
 
 	# try heuristics: 2
@@ -312,15 +337,36 @@ def reachable_addresses(program, w, equals, not_equals) :
 #	print("START")
 	for i in range(len(program)) :
 		if program[i]["l1"] == "hit" :
-			addresses += [program[i]["addr"]]
+			if not is_known(program[i]["addr"], addresses, equals):
+				addresses += [program[i]["addr"]]
 		elif program[i]["l1"] == "any" and is_known(program[i]["addr"], addresses, equals) :
 			pass
 		else :
 #			print("PUSH: ", addresses)
 			if program[i]["l1"] == "miss" :
-				not_equals += [[program[i]["addr"], a] for a in addresses]
-			result = result + [addresses] if len(addresses) > 0 else result
-			addresses = [program[j]["addr"] for j in range(max(0,i-w+1), max(0,i+1))]
+				addr = program[i]["addr"]
+				for a in addresses :
+					if ([addr, a] not in not_equals) and ([a, addr] not in not_equals) :
+						not_equals += [[a, addr]]
+			for j in range(i+1, len(program)) :
+				if program[j]["l1"] != "hit" : break
+				if always_different(program[j]["addr"], program[i]["addr"], equals, not_equals) and (
+					program[j]["addr"] not in addresses ) :
+					addresses += [program[j]["addr"]]
+
+			if len(addresses) > 0 :
+				result += [addresses]
+			
+			addresses = [program[i]["addr"]]
+			j = i
+			while j >= 0 and len(addresses) < w :
+				if is_known(program[j]["addr"], addresses, equals) :
+					pass
+				else :
+					addresses += [program[j]["addr"]]
+				j -= 1
+				
+			# addresses = [program[j]["addr"] for j in range(max(0,i-w+1), max(0,i+1))]
 #			print("ADDRESSES := ", addresses)
 			# TODO: add to "addresses" more addresses if has equals
 			# TODO: add "remvd"s to "addresses"
@@ -332,19 +378,24 @@ def reachable_addresses(program, w, equals, not_equals) :
 	return result
 
 def cliques(addresses, size, equals, not_equals) :
-	if len(addresses) <= size :
+	if len(addresses) < size :
 		return []
 
 	result = []
-	for i1 in range(len(addresses)) :
-		for i2 in range(i1+1, len(addresses)) :
+	print("addresses: ", addresses)
+	print("size: ", size)
+	for i1 in range(len(addresses) - size + 1) :
+		print("try: i1 =", i1)
+		for i2 in range(i1+1, len(addresses) - size+2) :
+			print("	try: i2 =", i2)
 			if always_different(addresses[i1], addresses[i2], equals, not_equals) :
 				if size == 2 :
 					result += [[addresses[i1], addresses[i2]]]
 					continue
 			else:
 				continue
-			for i3 in range(i2+1, len(addresses)) :
+			for i3 in range(i2+1, len(addresses) - size + 3) :
+				print("		try: i3 =", i3)
 				if always_different(addresses[i1], addresses[i2], equals, not_equals) and (
 				    always_different(addresses[i3], addresses[i2], equals, not_equals) ) :
 					if size == 3 :
@@ -352,7 +403,8 @@ def cliques(addresses, size, equals, not_equals) :
 						continue
 				else :
 					continue
-				for i4 in range(i3+1, len(addresses)) :		
+				for i4 in range(i3+1, len(addresses) - size + 4) :		
+					print("				try: i4 =", i4)
 					if always_different(addresses[i4], addresses[i1], equals, not_equals) and (
 					  always_different(addresses[i4], addresses[i2], equals, not_equals) ) and (
 					  always_different(addresses[i4], addresses[i3], equals, not_equals) ) :
@@ -361,7 +413,8 @@ def cliques(addresses, size, equals, not_equals) :
 							continue
 					else :
 						continue
-					for i5 in range(i4+1, len(addresses)) :			
+					for i5 in range(i4+1, len(addresses) - size + 5) :
+						print("					try: i5 =", i5)
 						if always_different(addresses[i5], addresses[i1], equals, not_equals) and (
 						always_different(addresses[i5], addresses[i2], equals, not_equals) ) and (
 						always_different(addresses[i5], addresses[i3], equals, not_equals) ) and (
@@ -381,26 +434,85 @@ def always_different(x, y, equals, not_equals) :
 
 def infer(addresses, equals, not_equals) :
 	result = False
-	for adrs in addresses :
+	for adrs in [s for s in addresses if len(s) > 4] :
+		print("see: ", adrs)
 		for clique in cliques(adrs, 4, equals, not_equals) :
-#			print("clique: ", clique)
+			print("clique: ", clique)
+			print("so tryes: ", [x for x in adrs if x not in clique])
 			for a in [x for x in adrs if x not in clique] : # for a in (adrs - clique)
+				print("a = ", a)
 				values = maybe_equal(clique, a, equals, not_equals)
-#				print("so ", a, "may be equal to ", values)
+				print("so ", a, "may be equal to ", values)
 				if values is not None and len(values) == 1 :
-					equals += [[values[0], a]]
+					if ([values[0], a] not in equals) and ([a, values[0]] not in equals) :
+						equals += [[values[0], a]]
 					print("INFER !!! ", values[0], " = ", a)
 					result = True
 #		else :
 #			print("no cliques :(")
+	print("all seen :)")
 	return result
 
 def maybe_equal(addresses, addr, equals, not_equals) :
+	print("maybe_equal: equals: ", equals)
+	print("maybe_equal: not_equals: ", not_equals)
 	result = []
 	for a in addresses :
 		if not always_different(a, addr, equals, not_equals) :
 			result += [a]
 	return result
+
+##################################################
+
+def infer_alldiffs(program, w, equals, not_equals) :  # returns (is_inferred : bool)
+	
+	inferred = False
+
+	for i in reversed([i for i in range(len(program))
+					if program[i]["l1"] == "miss" ]) :
+
+		seen = []
+		for j in reversed(range(i)) :
+			if program[j]["addr"] in seen : continue
+			seen += [program[j]["addr"]]
+
+			
+			if exists_path(program[i]["addr"], program[j]["addr"], equals) :
+				addresses = maybe_differents([program[k]["addr"] for k in range(j+1, i)], equals, not_equals)
+				if len(addresses) < w :
+					not_equals += [[program[i]["addr"], program[i]["addr"]]]  # unsat
+					return True
+				elif len(addresses) == w :
+					if program[i-1]["l1"] == "hit" :
+						not_equals += [[program[i]["addr"], program[i]["addr"]]] # unsat
+						return True
+					else :
+						program[i-1]["l1"] = "miss"
+						for i1 in range(len(addresses + [program[j]["addr"]])) :
+							for i2 in range(i1+1, len(addresses + [program[j]["addr"]])) :
+								a1 = program[i1]["addr"]
+								a2 = program[i2]["addr"]
+								if ([a1, a2] not in not_equals) and ([a2, a1] not in not_equals) :
+									not_equals += [[a1, a2]]
+									inferred = True
+				else :  # len(addresses) > w  # TODO
+					pass
+					
+
+	return inferred
+
+def maybe_differents(addresses, equals, not_equals) :
+	
+	if len(addresses) < 2 : return addresses
+
+	result = [addresses[0]]
+
+	for i in range(1, len(addresses)) :
+		if not is_known(addresses[i], result, equals) :
+			result += [addresses[i]]
+
+	return result
+
 
 #################################################
 
@@ -421,14 +533,25 @@ def run(length) :
 		print(x)
 	if not traverse_c(template, [1, 2, 3, 4], equals, not_equals) :
 		print("unsat")
+		return False
+	else :
+		return True
+
 
 if len(sys.argv) > 1 :
+	b = sys.argv[1]
 	while True :
 		start = time.time()
-		run(10)
+		c = run(10)
 		duration = time.time() - start
 		print(duration)
-		if duration > 10 : exit(0)
+		if duration > 10 :
+			if b == "unsat" :
+				if not c : exit(0)
+			elif b == "sat" :
+				if c : exit(0)
+			else :
+				exit(0)
 
 template1 = [{'addr': 'a2', 'l1': 'any'},
 {'addr': 'a1', 'l1': 'any'},
@@ -465,7 +588,7 @@ template1 = [
 {'addr': 'a2', 'l1': 'any'}
 ]
 
-template = [
+template1 = [
 {'addr': 'a8', 'l1': 'hit'},
 {'addr': 'a3', 'l1': 'any'},
 {'addr': 'a1', 'l1': 'hit'},
@@ -476,7 +599,151 @@ template = [
 {'addr': 'a7', 'l1': 'any'},
 {'addr': 'a0', 'l1': 'miss'},
 {'addr': 'a3', 'l1': 'miss'}
-]
+] # unsat
+
+template1 = [
+{'addr': 'a1', 'l1': 'any'},
+{'addr': 'a4', 'l1': 'miss'},
+{'addr': 'a1', 'l1': 'hit'},
+{'addr': 'a9', 'l1': 'any'},
+{'addr': 'a7', 'l1': 'miss'},
+{'addr': 'a2', 'l1': 'miss'},
+{'addr': 'a1', 'l1': 'any'},
+{'addr': 'a2', 'l1': 'hit'},
+{'addr': 'a4', 'l1': 'hit'},
+{'addr': 'a2', 'l1': 'any'}
+]  # sat
+
+template2222 = [
+{'addr': 'a9', 'l1': 'hit'},
+{'addr': 'a3', 'l1': 'any'},
+{'addr': 'a8', 'l1': 'any'},
+{'addr': 'a7', 'l1': 'hit'},
+{'addr': 'a5', 'l1': 'miss'},
+# {'addr': 'a1', 'l1': 'any' }, ###
+{'addr': 'a9', 'l1': 'miss'},
+{'addr': 'a4', 'l1': 'any'},
+{'addr': 'a5', 'l1': 'hit'},
+{'addr': 'a3', 'l1': 'hit'},
+{'addr': 'a4', 'l1': 'any'}
+]  # unsat
+
+template3 = [
+{'addr': 'a6', 'l1': 'any'},
+{'addr': 'a0', 'l1': 'hit'},
+{'addr': 'a0', 'l1': 'hit'},
+{'addr': 'a8', 'l1': 'miss'},
+{'addr': 'a0', 'l1': 'any'},
+{'addr': 'a6', 'l1': 'hit'},
+{'addr': 'a1', 'l1': 'hit'},
+{'addr': 'a0', 'l1': 'hit'},
+{'addr': 'a6', 'l1': 'any'},
+{'addr': 'a7', 'l1': 'any'}
+]  # sat
+
+template2 = [
+{'addr': 'a5', 'l1': 'any'},
+{'addr': 'a2', 'l1': 'hit'},
+{'addr': 'a2', 'l1': 'hit'},
+{'addr': 'a5', 'l1': 'hit'},
+{'addr': 'a4', 'l1': 'hit'},
+{'addr': 'a0', 'l1': 'miss'},
+{'addr': 'a7', 'l1': 'any'},
+{'addr': 'a2', 'l1': 'miss'},
+{'addr': 'a7', 'l1': 'hit'},
+{'addr': 'a5', 'l1': 'hit'}
+] # unsat
+
+template3 = [
+{'addr': 'a8', 'l1': 'miss'},
+{'addr': 'a9', 'l1': 'hit'},
+{'addr': 'a1', 'l1': 'hit'},
+{'addr': 'a9', 'l1': 'any'},
+{'addr': 'a1', 'l1': 'any'},
+{'addr': 'a1', 'l1': 'any'},
+{'addr': 'a1', 'l1': 'any'},
+{'addr': 'a5', 'l1': 'hit'},
+{'addr': 'a4', 'l1': 'any'},
+{'addr': 'a8', 'l1': 'any'}
+] # sat
+
+template3 = [
+{'addr': 'a6', 'l1': 'miss'},
+{'addr': 'a2', 'l1': 'hit'},
+{'addr': 'a0', 'l1': 'hit'},
+{'addr': 'a2', 'l1': 'hit'},
+{'addr': 'a6', 'l1': 'any'},
+{'addr': 'a5', 'l1': 'any'},
+{'addr': 'a9', 'l1': 'any'},
+{'addr': 'a4', 'l1': 'miss'},
+{'addr': 'a0', 'l1': 'miss'},
+{'addr': 'a6', 'l1': 'hit'}
+] # sat
+
+template2 = [
+{'addr': 'a0', 'l1': 'hit'},
+{'addr': 'a4', 'l1': 'hit'},
+{'addr': 'a7', 'l1': 'any'},
+{'addr': 'a9', 'l1': 'miss'},
+{'addr': 'a1', 'l1': 'hit'},
+{'addr': 'a3', 'l1': 'miss'},
+{'addr': 'a4', 'l1': 'miss'},
+{'addr': 'a7', 'l1': 'hit'},
+{'addr': 'a0', 'l1': 'hit'},
+{'addr': 'a5', 'l1': 'miss'}
+] # unsat
+
+template2 = [
+{'addr': 'a1', 'l1': 'hit'},
+{'addr': 'a6', 'l1': 'miss'},
+{'addr': 'a9', 'l1': 'miss'},
+{'addr': 'a9', 'l1': 'any'},
+{'addr': 'a2', 'l1': 'hit'},
+{'addr': 'a7', 'l1': 'miss'},
+{'addr': 'a3', 'l1': 'miss'},
+{'addr': 'a0', 'l1': 'miss'},
+{'addr': 'a6', 'l1': 'hit'},
+{'addr': 'a9', 'l1': 'hit'}
+] # unsat
+
+template3 = [
+{'addr': 'a2', 'l1': 'miss'},
+{'addr': 'a5', 'l1': 'miss'},
+{'addr': 'a0', 'l1': 'hit'},
+{'addr': 'a3', 'l1': 'any'},
+{'addr': 'a2', 'l1': 'any'},
+{'addr': 'a9', 'l1': 'any'},
+{'addr': 'a2', 'l1': 'any'},
+{'addr': 'a2', 'l1': 'hit'},
+{'addr': 'a5', 'l1': 'miss'},
+{'addr': 'a2', 'l1': 'any'}
+]  # sat
+
+template3 = [
+{'addr': 'a4', 'l1': 'any'},
+{'addr': 'a6', 'l1': 'hit'},
+{'addr': 'a1', 'l1': 'any'},
+{'addr': 'a7', 'l1': 'hit'},
+{'addr': 'a9', 'l1': 'hit'},
+{'addr': 'a7', 'l1': 'any'},
+{'addr': 'a1', 'l1': 'any'},
+{'addr': 'a1', 'l1': 'any'},
+{'addr': 'a1', 'l1': 'any'},
+{'addr': 'a1', 'l1': 'any'}
+] # sat
+
+template = [
+{'addr': 'a2' , 'l1': 'any'},
+{'addr': 'a3' , 'l1': 'any'},
+{'addr': 'a7' , 'l1': 'miss'},
+{'addr': 'a6' , 'l1': 'any'},
+{'addr': 'a1' , 'l1': 'hit'},
+{'addr': 'a5' , 'l1': 'miss'},
+{'addr': 'a4' , 'l1': 'hit'},
+{'addr': 'a9' , 'l1': 'miss'},
+{'addr': 'a7' , 'l1': 'miss'},
+{'addr': 'a6' , 'l1': 'any'}
+] # unsat
 
 # lru element is the last element of this seq
 initial_l1 = [1, 2, 3, 4]
